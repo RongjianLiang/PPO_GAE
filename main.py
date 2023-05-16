420738e6fca1fed88815a8e00be88338508ef8b4
RongjianLiang <rongjianliang67@gmail.com>
2023-05-16 17:24:09 +0800

    Implement the dataset class

--- main.py
+++ main.py
@@ -77,9 +77,43 @@ def test_agent(env, episode_length, policy, episodes=10):
 
     return sum(ep_returns) / episodes
 
+
 # TODO: create the policy
 
+class GradientPolicy(nn.Module):
+
+    def __init__(self, in_features, out_dims, hidden_size=128):
+        super().__init__()
+        self.fc1 = nn.Linear(in_features, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc_mu = nn.Linear(hidden_size, out_dims)
+        self.fc_std = nn.Linear(hidden_size, out_dims)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        loc = self.fc_mu(x)
+        loc = torch.tanh(loc)
+        scale = self.fc_std(x)
+        scale = F.softplus(scale) + 0.001
+        return loc, scale
+
+
 # TODO: create the value network
+
+class ValueNet(nn.Module):
+
+    def __init__(self, in_features, hidden_size=128):
+        super().__init__()
+        self.fc1 = nn.Linear(in_features, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, 1)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 
 class RunningMeanStd:
@@ -102,7 +136,7 @@ class RunningMeanStd:
 
 
 def update_mean_var_count_from_moments(
-    mean, var, count, batch_mean, batch_var, batch_count
+        mean, var, count, batch_mean, batch_var, batch_count
 ):
     delta = batch_mean - mean
     tot_count = count + batch_count
@@ -147,7 +181,76 @@ class NormalizeObservation(gym.core.Wrapper):
         return (obs - self.obs_rms.mean) / torch.sqrt(self.obs_rms.var + self.epsilon)
 
 
+# TODO: create the environment
+def create_env(env_name, num_envs=256, episode_length=1000):
+    env = gym.make(env_name, batch_size=num_envs, episode_length=episode_length)
+    env = to_torch.JaxToTorchWrapper(env, device=device)
+    env = NormalizeObservation(env)
+    return env
+
+
 # TODO: create the dataset
+class RLDataset(IterableDataset):
+    def __init__(self, env, policy, value_net, samples_per_epoch,
+                 gamma, lamb, repeats):
+        self.samples_per_epoch = samples_per_epoch
+        self.gamma = gamma
+        self.lamb = lamb
+        self.repeats = repeats
+        self.env = env
+        self.policy = policy
+        self.value_net = value_net
+        self.obs = self.env.reset()
+
+    @torch.no_grad()
+    def __iter__(self):
+        transitions = []
+        for step in range(self.samples_per_epoch):
+            loc, scale = self.policy(self.obs)
+            action = torch.normal(loc, scale)
+            next_obs, reward, done, info = self.env.step(action)
+            transitions.append((self.obs, loc, scale, action, reward, done, next_obs))
+            self.obs = next_obs
+
+        "create tensors for obs, loc, scale, action, reward, done, next_obs"
+        transitions = map(torch.stack, zip(*transitions))
+        obs_b, loc_b, scale_b, action_b, reward_b, done_b, next_obs_b = transitions
+        # right now the reward_b and done_b are in the shape of
+        # (samples_per_epoch, num_envs)
+        # add an extra dim to make them compatible with the rest
+        reward_b = reward_b.unsqueeze(dim=-1)
+        done_b = done_b.unsqueeze(dim=-1)
+
+        values_b = self.value_net(obs_b)
+        next_value_b = self.value_net(obs_b)
+
+        "create the batch of td errors"
+        td_error_b = reward_b + (1 - done_b) * self.gamma * next_value_b - values_b
+
+        "compute GAE"
+        running_gae = torch.zeros((self.env.num_envs, 1), dtype=torch.float32, device=device)
+        gae_b = torch.zeros_like(td_error_b)
+
+        for row in range(self.samples_per_epoch -1, -1, -1):
+            running_gae = td_error_b[row] + (1 - done_b[row]) * self.gamma * self.lamb * running_gae
+            gae_b[row] = running_gae
+
+        target_b = gae_b + values_b  # r_t + gamma * next_state_values?
+
+        "shuffle data and pass it to training"
+        num_samples = self.samples_per_epoch * self.env.num_envs
+        reshape_fn = lambda x: x.view(num_samples, -1)
+        batch = [obs_b, loc_b, scale_b, action_b, reward_b, gae_b, target_b]
+
+        obs_b, loc_b, scale_b, action_b, reward_b, gae_b, target_b = map(reshape_fn, batch)
+
+        for repeat in range(self.repeats):
+            idx = list(range(num_samples))
+            random.shuffle(idx)
+
+            for i in idx:
+                yield obs_b[i], loc_b[i], scale_b[i], action_b[i], reward_b[i], gae_b[i], target_b[i]
+
 
 # TODO: create PPO with Generalized Advantage Estimation
 def main():
@@ -157,8 +260,23 @@ def main():
     entry_point = functools.partial(envs.create_gym_env, env_name='halfcheetah')
     gym.register('brax-halfcheetah-v0', entry_point=entry_point)
 
-
-    pass
+    # env = gym.make("brax-halfcheetah-v0", episode_length=1000)
+    # env = to_torch.JaxToTorchWrapper(env, device=device)
+    # create_video(env, 1000)
+    env = create_env("brax-halfcheetah-v0", num_envs=10)
+    obs = env.reset()
+    # print("Num envs:    ", obs.shape[0], "Obs dimension:   ", obs.shape[1])
+    # print("Action space: ", env.action_space)
+    # next_obs, reward, done, info = env.step(env.action_space.sample())
+    # print("next obs: ", next_obs)
+    # print("\n")
+    # print("reward: ", reward)
+    # print("\n")
+    # print("done: ", done)
+    # print("\n")
+    # print("info: ", info)
+    # print("\n")
+    # print("info keys: ", info.keys())
 
 
 # Press the green button in the gutter to run the script.
